8118d67cfb140fb9ca4f1da4247e80fff06a8587
a0178378j <a0178378j@gmail.com>
2023-02-15 13:42:39 +0800

    updated app.js 2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Component } from "react";
-import { render } from "react-dom";
 
-class App extends Component {
-  render(){
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          I was changed
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
-    );
-  }
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <p className="App-intro">
+        I was changed!
+      </p>
+    </div>
+  );
 }
+
 export default App;
